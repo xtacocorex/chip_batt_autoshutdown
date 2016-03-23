da1af788a8041d08e8134a1505ed491bd12283cc
Robert Wolterman <robert.wolterman@gmail.com>
2016-03-22 20:32:04 -0500

    initial commit of the script, moving away from the github gist so people can file but reports against

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# chip_batt_autoshutdown
+CHIP Battery Auto-Shutdown
+============================
+
+This script will check the level of a battery attached to the CHIP.
+
+This needs to be run as root due to the shutdown command.
+
+This script does not have a loop internal to it and should be set to a cron job (preferably root cron) at a 5 or 10 minute interval.
+
+# Installation
+
+  ```
+  git clone https://github.com/xtacocorex/chip_batt_autoshutdown.git
+  cd chip_batt_autoshutdown.sh
+  sudo cp ./chip_batt_autoshutdown.sh /usr/bin/
+  ```
+
+Refer to Debian docs on setting up cron jobs
---
+++ chip_autoshutdown.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# 2016 ROBERT WOLTERMAN (xtacocorex)
+# WITH HELP FROM CHIP-hwtest/battery.sh
+
+# MIT LICENSE, SEE LICENSE FILE
+
+# THIS NEEDS TO BE RUN AS ROOT
+# PROBABLY SET AS A CRON JOB EVERY 5 OR 10 MINUTES
+
+# SIMPLE SCRIPT TO POWER DOWN THE CHIP BASED UPON BATTERY VOLTAGE
+
+# CHANGE THESE TO CUSTOMIZE THE SCRIPT
+# ** THESE MUST BE INTEGERS **
+MINVOLTAGELEVEL=2000
+MINCHARGECURRENT=10
+
+# TALK TO THE POWER MANAGEMENT
+i2cset -y -f 0 0x34 0x82 0xC3
+
+# GET POWER OP MODE
+POWER_OP_MODE=$(i2cget -y -f 0 0x34 0x01)
+
+# SEE IF BATTERY EXISTS
+BAT_EXIST=$(($(($POWER_OP_MODE&0x20))/32))
+
+if [ $BAT_EXIST == 1 ]; then
+
+    BAT_VOLT_MSB=$(i2cget -y -f 0 0x34 0x78)
+    BAT_VOLT_LSB=$(i2cget -y -f 0 0x34 0x79)
+    BAT_BIN=$(( $(($BAT_VOLT_MSB << 4)) | $(($(($BAT_VOLT_LSB & 0x0F)) )) ))
+    BAT_VOLT_FLOAT=$(echo "($BAT_BIN*1.1)"|bc)
+    # CONVERT TO AN INTEGER
+    BAT_VOLT=${$BAT_VOLT_FLOAT%.*}
+        
+    # CHECK BATTERY LEVEL AGAINST MINVOLTAGELEVEL
+    if [ $BAT_VOLT -le $MINVOLTAGELEVEL ]; then
+    
+        # GET THE CHARGE CURRENT
+        BAT_ICHG_MSB=$(i2cget -y -f 0 0x34 0x7A)
+        BAT_ICHG_LSB=$(i2cget -y -f 0 0x34 0x7B)
+        BAT_ICHG_BIN=$(( $(($BAT_ICHG_MSB << 4)) | $(($(($BAT_ICHG_LSB & 0x0F)) )) ))
+        BAT_ICHG_FLOAT=$(echo "($BAT_ICHG_BIN*0.5)"|bc)
+        # CONVERT TO AN INTEGER
+        BAT_ICHG=${$BAT_ICHG_FLOAT%.*}
+    
+        # IF CHARGE CURRENT IS LESS THAN MINCHARGECURRENT, WE NEED TO SHUTDOWN
+        if [ $BAT_ICHG -le $MINCHARGECURRENT ]; then
+            shutdown -h now
+        fi
+    fi
+
+fi
